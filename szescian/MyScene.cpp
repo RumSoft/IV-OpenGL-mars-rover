5b1eab63c44cf6f6a521b3c1c334724894d5e4f6
Dominik Ruman <dr124@o2.pl>
2019-03-23 22:25:13 +0100

    cleanup

--- szescian/MyScene.cpp
+++ szescian/MyScene.cpp
@@ -9,20 +9,6 @@
 MyScene::MyScene()
 {
 	input = InputHandler::GetInstance();
-	int a = 50;
-	auto s1 = Shape();
-	s1.Type = TriangleStrip;
-	s1.Points.emplace_back(0, 0, 0);
-	s1.Points.emplace_back(0, a, 0);
-	s1.Points.emplace_back(a, 0, 0);
-	s1.Points.emplace_back(a, a, 0);
-	s1.Points.emplace_back(a, 0, a);
-	s1.Points.emplace_back(a, a, a);
-	s1.Rotation = Quat::FromAngleAxis(Deg2Rad(30), Vec3(0, 0, 1));
-	auto g1 = new Geom();
-	g1->Shapes.push_back(s1);
-	this->Geometries.push_back(g1);
-
 	this->Geometries.push_back(new Grid());
 	this->Geometries.push_back(new Axes());
 }
