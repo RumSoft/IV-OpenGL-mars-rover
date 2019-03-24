cb5c160d771bf1287e4947c62aa7873e3489d4b9
Szumekk <szumskijk@gmail.com>
2019-03-24 23:50:47 +0100

    Update

--- szescian/Kolo.h
+++ szescian/Kolo.h
@@ -4,23 +4,22 @@
 class Kolo : public Geom
 {
 public:
-	float X, Y, Z, R, H;
-	float alpha = 0.0, PI = 3.14;
+	Vec3 point;
+	float R, H, alpha, PI;
 	//x,y,z jako pozycja srodkowa kola ;		r, h - wiadomo co i jak
-	Kolo(float x, float y, float z, float r, float h) : X(x), Y(y), Z(z), R(r), H(h)
+	Kolo(Vec3 p, float r, float h) : point(p), R(r), H(h), alpha(0.0), PI(3.14)
 	{
 		Vec3 P[2] = {
-			Vec3(x, y, z),
-			Vec3(x + h, y, z),
+			Vec3(point),
+			Vec3(point.X+h,point.Y,point.Z),
 		};
-
 		auto s1 = Shape(TriangleStrip, GREEN);					//zewnêtrzne ko³o
 		for (alpha = 0.0; alpha <= 2 * PI; alpha += PI / 8.0)
 		{
-			P[0].Y = R * sin(alpha) + y;
-			P[0].Z = R * cos(alpha) + z;
-			P[1].Y = R * sin(alpha) + y;
-			P[1].Z = R * cos(alpha) + z;
+			P[0].Y = R * sin(alpha) + point.Y;
+			P[0].Z = R * cos(alpha) + point.Z;
+			P[1].Y = R * sin(alpha) + point.Y;
+			P[1].Z = R * cos(alpha) + point.Z;
 			s1.Points.push_back(P[0]);
 			s1.Points.push_back(P[1]);
 		}
@@ -30,10 +29,10 @@ public:
 		auto s2 = Shape(TriangleStrip, BLUE);					//wewnêtrzne ko³o
 		for (alpha = 0.0; alpha <= 2 * PI; alpha += PI / 8.0)
 		{
-			P[0].Y = R * sin(alpha) + y;
-			P[0].Z = R * cos(alpha) + z;
-			P[1].Y = R * sin(alpha) + y;
-			P[1].Z = R * cos(alpha) + z;
+			P[0].Y = R * sin(alpha) + point.Y;
+			P[0].Z = R * cos(alpha) + point.Z;
+			P[1].Y = R * sin(alpha) + point.Y;
+			P[1].Z = R * cos(alpha) + point.Z;
 			s2.Points.push_back(P[0]);
 			s2.Points.push_back(P[1]);
 		}
--- szescian/Lazik.h
+++ szescian/Lazik.h
@@ -8,17 +8,23 @@ class Lazik : public Geom
 public:
 	Lazik()
 	{
+		float r = 7, h = 10;
+		Vec3 k[6] = {
+			Vec3(5,-18,-15),
+			Vec3(5, 0,-15),
+			Vec3(5, 18,-15),
+			Vec3(-15,-18,-15),
+			Vec3(-15,0,-15),
+			Vec3(-15,18,-15),
+		};
 		Geom* parts[] = {
-			new Kolo(5, -18, -15, 7, 10),
-			new Kolo(5, 0, -15, 7, 10),
-			new Kolo(5, 18, -15, 7, 10),
-			new Kolo(-15, -18, -15, 7, 10),
-			new Kolo(-15, 0, -15, 7, 10),
-			new Kolo(-15, 18, -15, 7, 10),
-			new Kadlubek(15, 25, 10)
-			//kolo1(Pozycjakola1)
-			//kolo2(pozycjakola2)
-			//mozesz dodac zmienne typu Vec3 określające pozycje kola
+			new Kolo(k[0],r,h),
+			new Kolo(k[1],r,h),
+			new Kolo(k[2],r,h),
+			new Kolo(k[3],r,h),
+			new Kolo(k[4],r,h),
+			new Kolo(k[5],r,h),	
+			new Kadlubek(15, 25, 10),
 		};
 		for(auto part : parts)
 			Shapes.insert(Shapes.end(), part->Shapes.begin(), part->Shapes.end());
