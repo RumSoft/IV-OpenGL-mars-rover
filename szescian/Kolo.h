e13acf2240165be327c2cd6b202c69712c7a98f8
Szumekk <szumskijk@gmail.com>
2019-03-24 23:59:41 +0100

    Update

--- szescian/Kolo.h
+++ szescian/Kolo.h
@@ -5,16 +5,17 @@ class Kolo : public Geom
 {
 public:
 	Vec3 point;
-	float R, H, alpha, PI;
+	float R, H;
 	//x,y,z jako pozycja srodkowa kola ;		r, h - wiadomo co i jak
-	Kolo(Vec3 p, float r, float h) : point(p), R(r), H(h), alpha(0.0), PI(3.14)
+	Kolo(Vec3 p, float r, float h) : point(p), R(r), H(h)
 	{
+		float alpha = 0.0;
 		Vec3 P[2] = {
 			Vec3(point),
-			Vec3(point.X+h,point.Y,point.Z),
+			Vec3(point.X + h,point.Y,point.Z),
 		};
 		auto s1 = Shape(TriangleStrip, GREEN);					//zewnêtrzne ko³o
-		for (alpha = 0.0; alpha <= 2 * PI; alpha += PI / 8.0)
+		for (alpha = 0.0; alpha <= 2 * M_PI; alpha += M_PI / 8.0)
 		{
 			P[0].Y = R * sin(alpha) + point.Y;
 			P[0].Z = R * cos(alpha) + point.Z;
@@ -25,14 +26,14 @@ public:
 		}
 		this->Shapes.push_back(s1);
 
-		R = R - R / 3;
+		float R2 = R - R / 3;
 		auto s2 = Shape(TriangleStrip, BLUE);					//wewnêtrzne ko³o
-		for (alpha = 0.0; alpha <= 2 * PI; alpha += PI / 8.0)
+		for (alpha = 0.0; alpha <= 2 * M_PI; alpha += M_PI / 8.0)
 		{
-			P[0].Y = R * sin(alpha) + point.Y;
-			P[0].Z = R * cos(alpha) + point.Z;
-			P[1].Y = R * sin(alpha) + point.Y;
-			P[1].Z = R * cos(alpha) + point.Z;
+			P[0].Y = R2 * sin(alpha) + point.Y;
+			P[0].Z = R2 * cos(alpha) + point.Z;
+			P[1].Y = R2 * sin(alpha) + point.Y;
+			P[1].Z = R2 * cos(alpha) + point.Z;
 			s2.Points.push_back(P[0]);
 			s2.Points.push_back(P[1]);
 		}
--- szescian/Lazik.h
+++ szescian/Lazik.h
@@ -23,10 +23,10 @@ public:
 			new Kolo(k[2],r,h),
 			new Kolo(k[3],r,h),
 			new Kolo(k[4],r,h),
-			new Kolo(k[5],r,h),	
+			new Kolo(k[5],r,h),
 			new Kadlubek(15, 25, 10),
 		};
-		for(auto part : parts)
+		for (auto part : parts)
 			Shapes.insert(Shapes.end(), part->Shapes.begin(), part->Shapes.end());
 	}
 };
