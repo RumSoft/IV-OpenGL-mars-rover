f066bd62952b01df024d7db0b389e6e85398b620
Dominik Ruman <dr124@o2.pl>
2019-03-26 23:57:35 +0100

    use emplace back

--- szescian/Kolo.h
+++ szescian/Kolo.h
@@ -14,28 +14,28 @@ public:
 		auto s1 = new Shape(TriangleStrip, ColorF(0.2, 0.2, 0.2));
 		for (int i = 0; i <= steps; i++)
 		{
-			s1->Points.push_back(Vec3(-h / 2, r * cos(i * f), r * sin(i * f)));
-			s1->Points.push_back(Vec3(-h / 2, r * cos((i + 1) * f), r * sin((i + 1) * f)));
+			s1->Points.emplace_back(-h / 2, r * cos(i * f), r * sin(i * f));
+			s1->Points.emplace_back(-h / 2, r * cos((i + 1) * f), r * sin((i + 1) * f));
 
-			s1->Points.push_back(Vec3(h / 2, r * cos(i * f), r * sin(i * f)));
-			s1->Points.push_back(Vec3(h / 2, r * cos((i + 1) * f), r * sin((i + 1) * f)));
+			s1->Points.emplace_back(h / 2, r * cos(i * f), r * sin(i * f));
+			s1->Points.emplace_back(h / 2, r * cos((i + 1) * f), r * sin((i + 1) * f));
 
-			s1->Points.push_back(Vec3(h / 2, r2 * cos(i * f), r2 * sin(i * f)));
-			s1->Points.push_back(Vec3(h / 2, r2 * cos((i + 1) * f), r2 * sin((i + 1) * f)));
+			s1->Points.emplace_back(h / 2, r2 * cos(i * f), r2 * sin(i * f));
+			s1->Points.emplace_back(h / 2, r2 * cos((i + 1) * f), r2 * sin((i + 1) * f));
 
-			s1->Points.push_back(Vec3(-h / 2, r2 * cos(i * f), r2 * sin(i * f)));
-			s1->Points.push_back(Vec3(-h / 2, r2 * cos((i + 1) * f), r2 * sin((i + 1) * f)));
+			s1->Points.emplace_back(-h / 2, r2 * cos(i * f), r2 * sin(i * f));
+			s1->Points.emplace_back(-h / 2, r2 * cos((i + 1) * f), r2 * sin((i + 1) * f));
 		}
 
 		auto s2 = new Shape(TriangleFan, ColorF(0.3, 0.3, 0.3));
-		s2->Points.push_back(Vec3(0.8 * h / 2, 0, 0));
+		s2->Points.emplace_back(0.8 * h / 2, 0, 0);
 		for (int i = 0; i <= steps; i++)
-			s2->Points.push_back(Vec3(0.9 * h / 2, r2 * cos(i * f), r2 * sin(i * f)));
+			s2->Points.emplace_back(0.9 * h / 2, r2 * cos(i * f), r2 * sin(i * f));
 
 		auto s3 = new Shape(TriangleFan, ColorF(0.3, 0.3, 0.3));
-		s3->Points.push_back(Vec3(0.8 * -h / 2, 0, 0));
+		s3->Points.emplace_back(0.8 * -h / 2, 0, 0);
 		for (int i = 0; i <= steps; i++)
-			s3->Points.push_back(Vec3(0.9 * -h / 2, r2 * cos(-i * f), r2 * sin(-i * f)));
+			s3->Points.emplace_back(0.9 * -h / 2, r2 * cos(-i * f), r2 * sin(-i * f));
 
 		s1->Origin = pos;
 		s2->Origin = pos;
