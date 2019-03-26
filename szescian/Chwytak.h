c7769b0b7d869b43d3d7512e335b521430ec9bea
Dominik Ruman <dr124@o2.pl>
2019-03-26 22:26:10 +0100

    Update Chwytak.h

--- szescian/Chwytak.h
+++ szescian/Chwytak.h
@@ -26,7 +26,6 @@ public:
 		s2->Origin = chwyts;
 		for (int i = 0; i <= steps; i++)
 			s2->Points.emplace_back(-h / 2, R * sin(f * i), R * cos(f * i));
-		this->Shapes.push_back(s2);
 
 		auto s3 = new Shape(TriangleFan, ColorF(0.3, 0.3, 0.3));
 		s3->Origin = chwyts;
@@ -34,7 +33,6 @@ public:
 		{
 			s3->Points.emplace_back(h / 2, R * sin(f * i), R * cos(f * i));
 		}
-		this->Shapes.push_back(s3);
 
 		float firstang = M_PI / 2; // Kat pierwszego ramienia MAX = M_PI/2; MIN = 0;
 		
@@ -49,23 +47,16 @@ public:
 			s5->Points.emplace_back(-h / 2, R * sin(f * i), R * cos(f * i));
 			s5->Points.emplace_back(h / 2, R * sin(f * i), R * cos(f * i));
 		}
-		this->Shapes.push_back(s5);
 
 		auto s6 = new Shape(TriangleFan, ColorF(0.3, 0.3, 0.3));
 		s6->Origin = chwyts + Vec3(0, cos(firstang)*hr, sin(firstang)*hr);
 		for (int i = 0; i <= steps; i++)
-		{
 			s6->Points.emplace_back(-h / 2, R * sin(f * i), R * cos(f * i));
-		}
-		this->Shapes.push_back(s6);
 
 		auto s7 = new Shape(TriangleFan, ColorF(0.3, 0.3, 0.3));
 		s7->Origin = chwyts + Vec3(0, cos(firstang)*hr, sin(firstang)*hr);;
 		for (int i = 0; i <= steps; i++)
-		{
 			s7->Points.emplace_back(h / 2, R * sin(f * i), R * cos(f * i));
-		}
-		this->Shapes.push_back(s7);
 
 		float secondang = M_PI / 4;																			//kat miedzy ramieniem 1 i 2
 		Vec3 chwyts2 = chwyts + Vec3(0, cos(firstang)*hr, sin(firstang)*hr);
@@ -84,28 +75,42 @@ public:
 		auto s9 = new Shape(TriangleFan, ColorF(0.3, 0.3, 0.3));
 		s9->Origin = chwyts2 + Vec3(0, cos(secondang)*hr, sin(secondang)*hr);
 		for (int i = 0; i <= steps; i++)
-		{
 			s9->Points.emplace_back(-h / 2, R * sin(f * i), R * cos(f * i));
-		}
 		this->Shapes.push_back(s9);
 
 		auto s10 = new Shape(TriangleFan, ColorF(0.3, 0.3, 0.3));
 		s10->Origin = chwyts2 + Vec3(0, cos(secondang)*hr, sin(secondang)*hr);
 		for (int i = 0; i <= steps; i++)
-		{
 			s10->Points.emplace_back(h / 2, R * sin(f * i), R * cos(f * i));
-		}
 		this->Shapes.push_back(s10);
+
+		this->Shapes.push_back(s1);
+		this->Shapes.push_back(s2);
+		this->Shapes.push_back(s3);
+		this->Shapes.push_back(s5);
+		this->Shapes.push_back(s6);
+		this->Shapes.push_back(s7);
+		this->Shapes.push_back(s8);
+		this->Shapes.push_back(s9);
+		this->Shapes.push_back(s10);
+
 
 		float thirdang = M_PI / 10;																			//kat chwytaka
 		Vec3 chwyts3 = chwyts2 + Vec3(0, cos(secondang)*hr, sin(secondang)*hr);
 		auto c1 = new Ramie(chwyts3, chwyts3 + Vec3(0, cos((M_PI / 6) + thirdang) * 10, sin((M_PI / 6) + thirdang) * 10), 3, 2, 0, RED);
-		parts.push_back(c1);
-		auto c2 = new Ramie(chwyts3 + Vec3(0, cos((M_PI / 6) + thirdang) * 10, sin((M_PI / 6) + thirdang) * 10), chwyts3 + Vec3(0, cos((M_PI / 6) + thirdang) * 10, sin((M_PI / 6) + thirdang) * 10) + Vec3(0, cos(M_PI / -6) * 10, sin(M_PI / -6) * 10), 3, 2, 0, GREEN);
-		parts.push_back(c2);
+		auto c2 = new Ramie(chwyts3 + Vec3(0, cos((M_PI / 6) + thirdang) * 10, sin((M_PI / 6) + thirdang) * 10),
+		                    chwyts3 + Vec3(0, cos((M_PI / 6) + thirdang) * 10, sin((M_PI / 6) + thirdang) * 10) + Vec3(
+			                    0, cos(M_PI / -6) * 10, sin(M_PI / -6) * 10), 3, 2, 0, GREEN);
 		auto c3 = new Ramie(chwyts3, chwyts3 + Vec3(0, cos((M_PI / -6) - thirdang) * 10, sin((M_PI / -6) - thirdang) * 10), 3, 2, 0, RED);
+		auto c4 = new Ramie(chwyts3 + Vec3(0, cos((M_PI / -6) - thirdang) * 10, 
+			sin((M_PI / -6) - thirdang) * 10),
+			chwyts3 + Vec3(0, cos((M_PI / -6) - thirdang) * 10, sin((M_PI / -6) - thirdang) * 10) +	
+			Vec3(0, cos(M_PI / 6) * 10, sin(M_PI / 6) * 10), 3, 2, 0, GREEN);
+
+
+		parts.push_back(c1);
+		parts.push_back(c2);
 		parts.push_back(c3);
-		auto c4 = new Ramie(chwyts3 + Vec3(0, cos((M_PI / -6) - thirdang) * 10, sin((M_PI / -6) - thirdang) * 10), chwyts3 + Vec3(0, cos((M_PI / -6) - thirdang) * 10, sin((M_PI / -6) - thirdang) * 10) + Vec3(0, cos(M_PI / 6) * 10, sin(M_PI / 6) * 10), 3, 2, 0, GREEN);
 		parts.push_back(c4);
 
 		for (auto part : parts)
