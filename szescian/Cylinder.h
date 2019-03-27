a42b1caccf60db26e7370615270b07c7e622691c
Dominik Ruman <dr124@o2.pl>
2019-03-27 01:08:51 +0100

    Update Cylinder.h

--- szescian/Cylinder.h
+++ szescian/Cylinder.h
@@ -4,7 +4,7 @@
 class Cylinder : public Shape
 {
 public:
-	Cylinder(Vec3 pos, float r, float h, int steps, ColorF color)
+	Cylinder(Vec3 pos, float r, float h, int steps, ColorF color, Quat rot = Quat::Identity(), bool center = true)
 	{
 		this->Type = TriangleStrip;
 		this->Origin = pos;
@@ -12,18 +12,18 @@ public:
 
 		float f = 2 * M_PI / steps;
 		for (int i = 0; i <= steps; i++) {
-			this->Points.push_back(Vec3(0, 0, -h / 2));
-			this->Points.push_back( Vec3(r *sin(i * f), r *cos(i*f), -h / 2));
+			this->Points.push_back(rot * Vec3(0, 0, center ? -h / 2 : 0));
+			this->Points.push_back(rot * Vec3(r *sin(i * f), r *cos(i*f), center ? -h / 2 : 0));
 		}
 
 		for (int i = 0; i <= steps; i++) {
-			this->Points.push_back(Vec3(r *sin(i * f), r *cos(i*f), -h / 2));
-			this->Points.push_back(Vec3(r *sin(i * f), r *cos(i*f), h / 2));
+			this->Points.push_back(rot * Vec3(r *sin(i * f), r *cos(i*f), center ? -h / 2 : 0));
+			this->Points.push_back(rot * Vec3(r *sin(i * f), r *cos(i*f), center ? h / 2 : h));
 		}
 
 		for (int i = 0; i <= steps; i++) {
-			this->Points.push_back(Vec3(r *sin(i * f), r *cos(i*f), h / 2));
-			this->Points.push_back(Vec3(0, 0, h / 2));
+			this->Points.push_back(rot * Vec3(r *sin(i * f), r *cos(i*f), center ? h / 2 : h));
+			this->Points.push_back(rot * Vec3(0, 0, h / 2));
 		}
 	}
 };
