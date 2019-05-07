8ec3d9e0943c62e658ee623deaa0fb538cf5066c
Dominik Ruman <dr124@o2.pl>
2019-05-08 00:48:25 +0200

    Update Kadlubek.h

--- szescian/Kadlubek.h
+++ szescian/Kadlubek.h
@@ -10,7 +10,7 @@ public:
 	float A, B, C;
 
 	Shape* top;
-	Shape* front, *right, *side3, *side4;
+	Shape* front, *right, *left, *back;
 	Shape* bottom;
 	Kadlubek(float a, float b, float c) : A(a), B(b), C(c)
 	{
@@ -26,18 +26,18 @@ public:
 			Vec3(0.7 * A,0.9 * B, C),
 		};
 		bottom = new Face(x[1], x[2], x[3], x[4], LIGHTGRAY);
-		front = new Face(x[8], x[4], x[6], x[2], LIGHTGRAY);
-		right = new Face(x[3], x[4], x[7], x[8], LIGHTGRAY);
+		front = new Face(x[4], x[8], x[2], x[6], LIGHTGRAY);
+		right = new Face(x[7], x[8], x[3], x[4],LIGHTGRAY);
 		top = new Face(x[7], x[8], x[5], x[6], LIGHTGRAY);
-		side3 = new Face(x[5], x[6], x[1], x[2], LIGHTGRAY);
-		side4 = new Face(x[1], x[3], x[5], x[7], LIGHTGRAY);
+		left = new Face(x[5], x[6], x[1], x[2], LIGHTGRAY);
+		back = new Face(x[5], x[7], x[1], x[3], LIGHTGRAY);
 
 		this->Shapes.push_back(front);
 		this->Shapes.push_back(bottom);
 		this->Shapes.push_back(top);
 		this->Shapes.push_back(right);
-		this->Shapes.push_back(side3);
-		this->Shapes.push_back(side4);
+		this->Shapes.push_back(left);
+		this->Shapes.push_back(back);
 	}
 
 	void Init() override
@@ -45,8 +45,8 @@ public:
 		top->Vertices[1].TextureCoordinate *= 2;
 		top->Vertices[3].TextureCoordinate.Y *= 2;
 		top->texture = TextureMemory::Instance().GetTexture("textures/ProjektLogo.png");
-		side3->texture = TextureMemory::Instance().GetTexture("textures/Metalic.png");
-		side4->texture = TextureMemory::Instance().GetTexture("textures/Metalic.png");
+		left->texture = TextureMemory::Instance().GetTexture("textures/Metalic.png");
+		back->texture = TextureMemory::Instance().GetTexture("textures/Metalic.png");
 		bottom->texture = TextureMemory::Instance().GetTexture("textures/Metalic.png");
 		front->texture = TextureMemory::Instance().GetTexture("textures/Metalic.png");
 		right->texture = TextureMemory::Instance().GetTexture("textures/Metalic.png");
