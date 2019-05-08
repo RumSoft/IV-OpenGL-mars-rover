cfbb1d49698316aba81b8018ab7489e6321b7955
Dominik Ruman <dr124@o2.pl>
2019-05-08 14:28:50 +0200

    Update Camera.h

--- szescian/Camera.h
+++ szescian/Camera.h
@@ -1,17 +1,22 @@
 #pragma once
 #include "Geom.h"
 
+
 class Camera : public Geom
 {
 public:
 	Geom* entity;
 	InputHandler* input;
+	
+	// animation
 	double t = 100;
 	double anim_t = 0.3;
 	Quat from;
 	Quat to;
+	
 	double zoom = 1;
 	Vec3 Offset = Vec3(-50, -100, 70);
+	
 	Camera(Geom* ent)
 	{
 		entity = ent;
@@ -35,17 +40,17 @@ public:
 			Rotation = Quat::Lerp(from, to, sinf(f * 3.14 * 0.5f));
 		}
 
-		if (input->IsDown(VK_LEFT))
-			RequestRotationBy(Quat::FromAngleAxis(D2R(30), axisZ));
-
-		if (input->IsDown(VK_RIGHT)) 
-			RequestRotationBy(Quat::FromAngleAxis(D2R(-30), axisZ));
-
 		if (input->IsDown('X'))
 			RequestRotationTo(entity->Rotation);
 
 		if (input->IsDown('V'))
 			RequestRotationTo(entity->Rotation * Quat::FromAngleAxis(D2R(-120), axisZ));
+
+		if (input->IsDown(VK_LEFT))
+			RequestRotationBy(Quat::FromAngleAxis(D2R(30), axisZ));
+
+		if (input->IsDown(VK_RIGHT))
+			RequestRotationBy(Quat::FromAngleAxis(D2R(-30), axisZ));
 
 		if (input->IsDown(VK_UP))
 			zoom -= 1 * frametime;
